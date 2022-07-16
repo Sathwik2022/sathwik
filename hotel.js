8f1ab95bc020170ab3558accc050d3939bf3f423
Sathwik2022 <106377400+Sathwik2022@users.noreply.github.com>
2022-07-16 14:57:59 +0530

    Add files via upload

--- hotel.js
+++ hotel.js
@@ -1,70 +1,23 @@
-const Item = require("../models/hotel");
+const express = require("express");
+const router = express.Router();
 
-exports.getItemById = (req, res, next, itemId) => {
-    Item.findById(itemId).exec((err, item) => {
-        if (err || !item) {
-            return res.status(400).json({
-                error: "404 item not found",
-            }); 
-        }
-        req.item = item;
-        next();
-    });
-};
+const {
+    createItem,
+    getItemById,
+    getItem,
+    deleteItem,
+    getAllItems,
+    updateItem,
+} = require("../controllers/hotel");
 
-exports.getAllItems = (req, res) => {
-    Item.find()
-        .sort("-createdAt")
-        .exec((err, items) => {
-            if (err || !items) {
-                return res.status(400).json({
-                    error: "something went wrong in finding all items",
-                });
-            }
-            res.json(items);
-        });
-};
+router.param("itemId", getItemById);
+router.get("/items/", getAllItems);
+router.get("/item/:itemId/", getItem);
 
-exports.getItem = (req, res) => {
-    return res.json(req.item);
-};
+router.post("/item/create/", createItem);
 
-exports.createItem = (req, res) => {
-    const item = new Item(req.body);
-    item.save((err, name) => {
-        if (err || !name) {
-            return res.status(400).json({
-            error: "something went wrong",
-        });
-    }
-    res.json({ name });
-    });
-};
+router.put("/item/:itemId/update", updateItem);
 
-exports.updateItem = (req, res) => {
-    const item = req.item;
-    item.name = req.body.name;
-    item.save((err, t) => {
-        if (err || !t) {
-            return res.status(400).json({
-                error: "something went wrong while updating",
-            });
-        }
-        res.json(t);
-    });
-};
+router.delete("/item/:itemId/delete", deleteItem);
 
-exports.deleteItem = (req, res) => {
-    const item = req.item;
-    item.remove((err, name) => {
-        if (err || !name) {
-            return res.status(400).json({
-                error: "something went wrong while deleting the menu",
-            });
-        }
-        res.json({
-            menu_deleted: name,
-            message: "Name deleted successfully!",
-        });
-    });
-};
+module.exports = router
